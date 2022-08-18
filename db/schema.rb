5adafe309a79a03abc8cacdc23eb8194d327bb3c
sarazaToussaint <tmsaraza@gmail.com>
2022-08-18 13:32:39 +0200

    Generate comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_105734) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_112851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "Text"
+    t.bigint "users_id", null: false
+    t.bigint "posts_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "Title"
@@ -34,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_105734) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "users", column: "users_id"
   add_foreign_key "posts", "users", column: "users_id"
 end
