49ee82fb1ac620c3abae984ed37dac826df81831
sarazaToussaint <tmsaraza@gmail.com>
2022-08-18 13:24:05 +0200

    Genetate posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_091943) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_105734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "Title"
+    t.text "Text"
+    t.integer "CommentsCounter"
+    t.integer "LikesCounter"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "Name"
@@ -23,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_091943) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users", column: "users_id"
 end
