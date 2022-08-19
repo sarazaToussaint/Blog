14092f5d59938c645e328433dd140db00b8bf0ef
sarazaToussaint <tmsaraza@gmail.com>
2022-08-19 21:36:02 +0200

    Add methods to update likes and comments counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'authorId'
   has_many :comments, class_name: 'Comment', foreign_key: 'authorId'
   has_many :likes, class_name: 'Like', foreign_key: 'authorId'
+
+  after_save :update_counter
+
+  def update_counter
+    user.increment!(:PostsCounter)
+  end  
+ 
+  def recent_comment
+    comments.order(created_at: :desc).limit(5)  
+  end 
+
 end
