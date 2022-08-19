d09b393ab5b4c54517ae69390c040306d6562564
sarazaToussaint <tmsaraza@gmail.com>
2022-08-19 20:24:45 +0200

    Add association to the tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :User
+  has_many :comments, class_name: 'Comment', foreign_key: 'authorId'
+  has_many :likes, class_name: 'Like', foreign_key: 'authorId'
 end
