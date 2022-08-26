c24b20323a0e226d6f44912ddccb7de005d8c198
sarazaToussaint <tmsaraza@gmail.com>
2022-08-26 13:49:54 +0200

    Make a final touch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[ :authorId])
-    @user_posts = @user.recent_post
+    @user = User.find(params[ :user_id])
+    @posts = @user.posts
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @recent_post = @user.recent_post
+    # @recent_post = @user.recent_post
   end
 end
