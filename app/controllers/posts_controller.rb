229825cbf6e372b94ab66a483cb02ac07f78ad6c
sarazaToussaint <tmsaraza@gmail.com>
2022-08-27 09:27:34 +0200

    Make changes in post-conntroller file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,26 +6,47 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comment = Comment.new
-    @like = Like.new
   end
 
   def new
     @post = Post.new
-    @params = params
   end
-  
+
   def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to user_post_path(id: @post.id, user_id: @post.authorId), notice: 'Post was created successfully.'
-    else
-      render :new, alert: 'Error occured, Post not saved.'  
-  end  
+    @post = Post.create(post_params)
+    @post.user = current_user
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_post_path(current_user, @post)
+        else
+          flash.now[:error] = 'Error occured, Post not saved.'
+          render :new
+        end
+      end
+    end
+  end
 
   private
 
   def post_params
-    params.require(:post).permit( :Title, :Text, :authorId) 
-  end  
+    params.require(:post).permit(:Title, :Text)
+  end
 end
+  
+#   def create
+#     @post = Post.new(post_params)
+#     if @post.save
+#       redirect_to user_post_path(id: @post.id, user_id: @post.authorId), notice: 'Post was created successfully.'
+#     else
+#       render :new, alert: 'Error occured, Post not saved.'  
+#   end  
+
+#   private
+
+#   def post_params
+#     params.require(:post).permit( :Title, :Text, :authorId) 
+#   end  
+# end
+# end
